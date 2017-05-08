188cd65e0d27200ad949398a30dff810ecaf4f54
Harper Frankstone <Harper Frankstone>
2017-05-08 11:26:54 -0500

    started to build out the activate_customer and get_active_customer, but need to merge a recent push from jordan to test my code

--- cli/admintasks.py
+++ cli/admintasks.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 def write_customer_to_database(self, name, address, city, state, postal_code, telephone):
 
     return 1 
@@ -7,12 +9,36 @@ def read_from_customer_table(self, table_name, property, id):
     return 1 
 
 def activate_customer(self, id):
+    """
+    Purpose: to set the boolean value of the selected customer to true
+    Author: Harper Frankstone
+    Args: id - (integer) the customer id, used to indicate which customer to set as active 
+    Return: n/a
+    """
+    with sqlite3.connect('db.db') as conn:
+        c = conn.cursor()
 
-    return 1
+        try:
+           c.execute("""insert into customer [(active)] where customer_id is {} values {}""".format(id, 1))
+        except sqlite3.OperationalError:
+           pass
+
+        conn.commit()
 
 def get_active_customer(self):
+    """
+    Purpose: to show the active customer 
+    Author: Harper Frankstone
+    Args: n/a
+    Return: the name of the active customer
+    """
 
-    return 1
+    with sqlite3.connect('db.db') as conn:
+      c = conn.cursor()
+
+      c.execute("select name from customer where active is {} order by customer_id desc limit 1".format(1))
+      print(c.fetchone())
+
 
 def create_payment_type(self, payment_type_name, account_number, payment_type_id):
 
