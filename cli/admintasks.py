b2e1cf666849e7c1219c5415a9da5a7b8be2df03
Casey Dailey <casey@feasttogether.org>
2017-05-08 13:20:23 -0500

    method to update payment type on order is in place

--- cli/admintasks.py
+++ cli/admintasks.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 def write_customer_to_database(self, name, address, city, state, postal_code, telephone):
 
     return 1 
@@ -35,15 +37,32 @@ def get_order(self, customer_id):
     return "Diapers"
 
 def assign_payment_type_to_customer_order(self, order_id, payment_id):
+    """Purpose: assigning a payment type to a customer's order will 'complete' the order
+    Author: Casey Dailey
+    Args: order_id=an integer, foreign key to the payment_type table specifies the order to be updated, 
+          payment_id=the value corresponding to a customer's particular payment type
+    Returns: N/A
+    """
+    with sqlite3.connect('../db.db') as conn:
+        c = conn.cursor()
 
-    pass
+    c.execute("""update order 
+                 set payment_type_id={} 
+                 where order_id={};""".format(payment_id, order_id))
 
-def  read_from_order_table(self, table_name, table_property, column_id):
+    conn.commit()
+
+
+def read_from_order_table(self, table_name, table_property, column_id):
 
     return 1 
 
 def read_top_three_products(self):
 
     return ['Diaper']
+
+
+
+
 
 
