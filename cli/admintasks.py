ed2dcd6116d00a07e5199df45598cc334d5fd63d
James Tonkin <jameswtonkin@gmail.com>
2017-05-08 15:12:59 -0500

    created function to add products to orders and productorder table.

--- cli/admintasks.py
+++ cli/admintasks.py
@@ -1,10 +1,12 @@
+import sqlite3
+
 def write_customer_to_database(self, name, address, city, state, postal_code, telephone):
 
-    return 1 
+    return 1
 
 def read_from_customer_table(self, table_name, property, id):
 
-    return 1 
+    return 1
 
 def activate_customer(self, id):
 
@@ -20,19 +22,59 @@ def create_payment_type(self, payment_type_name, account_number, payment_type_id
 
 def get_payment_types(self, customer_id):
 
-    return 1 
+    return 1
 
 def read_inventory(self):
+    """ Query products, store as a list, and print to command line so customer can select a product to add to their order.
 
-    return ["Diapers"]
+    Arguments: None.
 
-def add_product_to_customer_order(self, product, customer_id):
+    Author: James Tonkin """
 
-    pass 
+    with sqlite3.connect('../db.db') as conn:
+        c = conn.cursor()
 
-def get_order(self, customer_id):
+        c.execute("SELECT ProductName FROM Product")
+        inventory = c.fetchall()
 
-    return "Diapers"
+        try:
+            if inventory != None:
+                return inventory
+            else:
+                raise TypeError
+        except TypeError:
+            return None
+
+def create_order(customer_id):
+    """ Creates a new order.
+
+    Arguments: customer_id that the order is for.
+
+    Author: James Tonkin """
+
+    with sqlite3.connect('../db.db') as conn:
+        c = conn.cursor()
+
+        c.execute("INSERT INTO Orders VALUES (?, ?, ?)",
+                    (None, customer_id, None ))
+
+        conn.commit()
+
+def add_product_to_customer_order(product_id, order_id):
+    """ Method to add products to a customer order.
+
+    Arguments: product_id that was ordered, order_id that is the product is being added to.
+
+    Author: James Tonkin """
+
+    with sqlite3.connect('../db.db') as conn:
+        c = conn.cursor()
+
+        c.execute("INSERT INTO ProductOrder VALUES (?, ?, ?)",
+                    (None, product_id, order_id))
+
+        conn.commit()
+        return c.lastrowid
 
 def assign_payment_type_to_customer_order(self, order_id, payment_id):
 
@@ -40,10 +82,8 @@ def assign_payment_type_to_customer_order(self, order_id, payment_id):
 
 def  read_from_order_table(self, table_name, table_property, column_id):
 
-    return 1 
+    return 1
 
 def read_top_three_products(self):
 
     return ['Diaper']
-
-
--- test/TestSweet.py
+++ test/TestSweet.py
@@ -22,37 +22,32 @@ class TestSweet(unittest.TestCase):
         self.assertEqual(active_customer, 1)
 
     def test_user_can_add_payment_type_to_customer_account(self):
-        # create_payment_type will insert into the payment type table the values from the user's input, the '1' in the assertion is referring to the customer id 
+        # create_payment_type will insert into the payment type table the values from the user's input, the '1' in the assertion is referring to the customer id
         admintasks.create_payment_type(self, payment_type_name='Visa', account_number=123456, payment_type_id=1)
         payment_type = admintasks.get_payment_types(self, customer_id=1)
 
-        self.assertEqual(payment_type, 1) 
+        self.assertEqual(payment_type, 1)
 
-            
+
     def test_user_can_add_product_to_customer_order(self):
-        # read_inventory will query the database for the list of products and print them to the command line. Inventory is a list of dictionaries, storing the where the product name is the key and the product's price is the value
-        inventory = admintasks.read_inventory(self)
-        product = inventory[0]
-        # add_product_to_customer_order will add the product to the order table in the database 
-        admintasks.add_product_to_customer_order(self, product='Diapers', customer_id=1)
-        order = admintasks.get_order(self, customer_id=1)
+        # add_product_to_customer_order will add the product to the order table in the database
+        productorder = admintasks.add_product_to_customer_order(1, 1)
 
-        self.assertIsNotNone(inventory)
-        self.assertIn('Diapers', order)
+
+        self.assertIsNotNone(productorder)
 
 
     def test_user_can_complete_customer_order(self):
         # within this method, the order table will be updated with a payment type id, signalling its completion. The method should accept, as arguments, an order id and a payment type id
         admintasks.assign_payment_type_to_customer_order(self, order_id=1, payment_id=1)
-        # the general read_from_table method will accept a table name, a column id that we want to reference and a property as arguments, using those arguments to pass into the database query 
+        # the general read_from_table method will accept a table name, a column id that we want to reference and a property as arguments, using those arguments to pass into the database query
         completed_order = admintasks.read_from_order_table(self, table_name='order', table_property='order.payment_type_id', column_id=1)
-        
+
         self.assertEqual(completed_order, 1)
 
 
     def test_user_can_see_product_popularity(self):
-        
+
         top_products = admintasks.read_top_three_products(self)
 
         self.assertIn('Diaper', top_products)
-
