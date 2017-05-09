c64d81f635f99c97f1731360dd85ce93b31f98cd
“Aaron <barfootaaron@yahoo.com>
2017-05-09 14:12:40 -0500

    added create_payment_option_cli method

--- cli/command_line_functions.py
+++ cli/command_line_functions.py
@@ -54,7 +54,19 @@ def activate_a_customer_cli():
 			break
 
 def create_payment_option_cli():
-	pass
+	'''create_payment_option_cli, author: Aaron Barfoot
+	Allows user to add a payment type to their account and writes input to database
+	-----------------
+	None
+	'''
+	active_customer = get_active_customer()
+
+	payment_type_name = input('Enter name of payment type:\n> ')
+	account_number = input('Enter account number for payment type:\n> ')
+	customer_id = active_customer
+
+	create_payment_type(payment_type_name, account_number, customer_id)
+	
 
 def add_product_to_cart_cli():
 	pass
