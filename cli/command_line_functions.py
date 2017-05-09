5b9a0e358bc2f1ddcff3058c931d155383720c34
Jordan Nelson <jnelsontn@gmail.com>
2017-05-09 13:32:32 -0500

    added deactivate customer and ability to activate customer from cli

--- cli/command_line_functions.py
+++ cli/command_line_functions.py
@@ -2,7 +2,12 @@ from admintasks import *
 from command_line_functions import *
 
 def create_customer_cli():
-
+	'''create_customer_cli, author: Jordan Nelson
+    Allows the user to input a customer's information which
+    is then written to the database
+    ----------------
+	None
+    '''
 	name = input('Enter customer name:\n> ')
 	address = input('Enter street address:\n> ')
 	city = input('Enter city:\n> ')
@@ -11,3 +16,51 @@ def create_customer_cli():
 	phone_number = input('Enter phone number:\n> ')
 
 	write_customer_to_database(name, address, city, state, postal_code, phone_number, active=0)
+
+def activate_customer_cli():
+	'''active_customer_cli, author: Jordan Nelson
+	Calls the get_active_customer method. If anything other than 'None' is returned
+	the user recieves a message that a customer is already active in the system.
+    ----------------
+	None
+    '''
+	active_customer = get_active_customer()
+
+	if active_customer == None:
+		activate_a_customer_cli()
+	else:
+		deactivate_customer(active_customer)
+		activate_a_customer_cli()
+
+def activate_a_customer_cli():
+	'''activate_a_customer_cli, author: Jordan Nelson
+	Presents the user with a list of all customers, by choosing the number of the
+	customer, the specific customer is marked as active.
+    ----------------
+	None
+    '''
+	print('Which customer will be active?\n')
+
+	customers =  get_customer_list()
+	for custid, name in customers:
+		print(str(custid) + '. ' + name)
+
+	user_input = input('> ')
+	user_input = int(user_input)
+
+	for custid, name in customers:
+		if user_input == custid:
+			activate_customer(user_input)
+			break
+
+def create_payment_option_cli():
+	pass
+
+def add_product_to_cart_cli():
+	pass
+
+def complete_order_cli():
+	pass
+
+def product_popularity_cli():
+	pass
