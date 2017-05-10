1426c250857282553f24432da50305458e618143
Jordan Nelson <jnelsontn@gmail.com>
2017-05-10 10:28:30 -0500

    changed the way activate customer list is created, now sequentially

--- cli/admintasks.py
+++ cli/admintasks.py
@@ -110,8 +110,6 @@ def get_active_customer():
         except TypeError:
             return None
 
-        conn.commit()
-
 def create_payment_type(self, payment_type_name, account_number, customer_id):
     '''create_payment_type, author: Aaron Barfoot
     Creates a new payment type in database and assign to active user
